a2ee3805472326fb70361ff0c522d4870089a6b5
avistheripper <bboyxns@mail.ru>
2019-03-17 21:21:17 +0300

    nav component + layout

--- codepost/src/app/app.module.ts
+++ codepost/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostService } from './services/post.service';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
